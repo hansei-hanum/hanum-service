1c0a7c6f5847d307b3c59d9b9d86bf63c8319435
Soju06 <qlskssk@gmail.com>
2024-03-16 00:31:43 +0900

    feat: add hanum community

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ sudo docker compose build
 
 read -p "Press enter to continue"
 
+mkdir -p .data/community/media
+mkdir -p .logs/community
+mkdir -p .logs/pay
+
+sudo chown -R hanum_app:hanum_app .data
+sudo chown -R hanum_app:hanum_app .logs
+
 sudo docker compose up -d
